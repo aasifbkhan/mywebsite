63e15f41e88535de28e8cec2963875eeadbc7d9a
aasifbkhan <aasifbkhan98@gmail.com>
2020-02-04 20:48:03 +0530

    fourth commit

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'fysd_7&j9c@g9i4mkf4hj$*j(1koll+h*#q25d5ailuvr2!%+#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
